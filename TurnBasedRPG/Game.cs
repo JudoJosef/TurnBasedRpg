3157880db961a494d83c918e7a051cced4b8dc29
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-10 11:55:30 +0100

    Add champion selection

--- TurnBasedRPG/Game.cs
+++ TurnBasedRPG/Game.cs
@@ -6,5 +6,30 @@ namespace TurnBasedRPG
 {
     internal class Game
     {
+        private List<Champion> _champions = new List<Champion>();
+        private List<IMonster> _monsters = new List<IMonster>();
+        private List<ICreature> _creatures = new List<ICreature>();
+
+        public void Run()
+        {
+            GetChampions();
+        private void GetChampions()
+        {
+            var selectedChampions = Draw.SelectChampions();
+            selectedChampions.ForEach(championType => _champions.Add(SummonChampion(championType)));
+        }
+        private Champion SummonChampion(ClassTypes type)
+            => type switch
+            {
+                ClassTypes.Archer => Portal.SummonArcher(),
+                ClassTypes.Assassin => Portal.SummonAssassin(),
+                ClassTypes.Dryad => Portal.SummonDryad(),
+                ClassTypes.Fighter => Portal.SummonFighter(),
+                ClassTypes.Jojo => Portal.CallJojo(),
+                ClassTypes.Mage => Portal.SummonMage(),
+                ClassTypes.Paladin => Portal.SummonPaladin(),
+                ClassTypes.Swordsman => Portal.SummonSwordsman(),
+                _ => throw new NotImplementedException()
+            };
     }
 }
