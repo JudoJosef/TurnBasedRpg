e7f9857a62861e2c083eeb755d4343f960a2a3a8
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-10 11:51:44 +0100

    Add IChampionSkills interface

---
+++ TurnBasedRPG/Classes/Skills/IChampionSkills.cs
@@ -0,0 +1,19 @@
+﻿namespace TurnBasedRPG.Classes.Skills
+{
+    internal interface IChampionSkills
+    {
+        static IEnumerable<Skill> GetSkills()=> throw new NotImplementedException();
+
+        static void UseFirstSkill(Champion champion, List<ICreature> creatures) => throw new NotImplementedException();
+
+        static void UseSecondSkill(Champion champion, List<ICreature> creatures) => throw new NotImplementedException();
+
+        static void UseThirdSkill(Champion champion, List<ICreature> creatures) => throw new NotImplementedException();
+
+        static Skill GetFirstSkill() => throw new NotImplementedException();
+
+        static Skill GetSecondSkill() => throw new NotImplementedException();
+
+        static Skill GetThirdSkill() => throw new NotImplementedException();
+    }
+}
