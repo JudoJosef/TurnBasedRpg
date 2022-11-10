205310baaf6f34adca555ced379a722b3de253ea
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-10 17:08:34 +0100

    Add Item class

---
+++ TurnBasedRPG/Lobby/ItemNames.cs
@@ -0,0 +1,44 @@
+﻿namespace TurnBasedRPG.Lobby
+{
+    public enum HelmetNames
+    {
+        Caesura,
+        DemonicEmbrace,
+        SpectresCowl,
+    }
+
+    public enum ChestplateNames
+    {
+        SpiritVisage,
+        ForgefireCrest,
+        Equinox,
+    }
+
+    public enum LegginsNames
+    {
+        WingedMoonplate,
+        HextechRocketbelt,
+        EdgeOfNight,
+    }
+
+    public enum BootNames
+    {
+        PlatedSteelcaps,
+        BootsOfSwiftness,
+        SorcerersShoes,
+    }
+
+    public enum WeaponNames
+    {
+        Syzygy,
+        Bloodward,
+        Deicide,
+    }
+
+    public enum AccessoireNames
+    {
+        MoonstoneRenewer,
+        ZhonyasHourglass,
+        DarkSeal
+    }
+}
