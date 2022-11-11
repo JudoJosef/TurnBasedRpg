92701b9b4d9d330d1282110343087efd3bc6383e
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-11 14:06:34 +0100

    Implement sell logic in Shop class

--- TurnBasedRPG/Lobby/Shop.cs
+++ TurnBasedRPG/Lobby/Shop.cs
@@ -2,5 +2,40 @@
 {
     internal class Shop
     {
+        private static void Sell(SummonerInventory inventory)
+        {
+            string selected = string.Empty;
+
+            while (selected != _backOption)
+            {
+                Draw.Clear();
+                selected = Draw.SelectSingle(inventory.Items.Select(kvp => $"#{kvp.Key} {kvp.Value.Name}").Concat(new List<string>{ "Back" }), "Select item to sell.");
+
+                if (selected != _backOption)
+                    TrySellItem(selected, inventory);
+            }
+        }
+        private static void TrySellItem(string selected, SummonerInventory inventory)
+        {
+            var selectedItem = GetItem(selected, inventory.Items);
+            Draw.WriteItemTable(new List<Item> { selectedItem });
+            selected = Draw.SelectSingle(new List<string> { _sellOption, _backOption }, "Select action.");
+
+            if (selected == _sellOption)
+            {
+                inventory.Gold += selectedItem.Price;
+                inventory.Items.Remove(inventory.Items.Where(kvp => kvp.Value == selectedItem).Single().Key);
+            }
+        }
+
+        private static Item GetItem(string name)
+            => _items.Where(item => item.Name == name)
+                .Single();
+
+        private static Item GetItem(string selected, Dictionary<int, Item> items)
+            => items[int.Parse(
+                selected.Split(" ")
+                .First()
+                .Replace("#", string.Empty))];
     }
 }
