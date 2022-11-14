cc179180d16af685d6081ddb62883d16f13d5f46
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-14 14:02:54 +0100

    Add EnterForge function

--- TurnBasedRPG/Lobby/Forge.cs
+++ TurnBasedRPG/Lobby/Forge.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TurnBasedRPG.Lobby.Items;
+using TurnBasedRPG.Player;
 
 namespace TurnBasedRPG.Lobby
 {
     internal class Forge
     {
+        public static void EnterForge(Summoner summoner)
+        {
+            string selected = string.Empty;
+
+            while (selected != _backOption)
+            {
+                Draw.Clear();
+                selected = Draw.SelectSingle(new List<string> { _upgradeOption, _craftOption, _backOption }, "You have entered the forge.");
+                if (selected == _upgradeOption)
+                    Upgrade(summoner);
+                else if (selected == _craftOption)
+                    Craft(summoner.Inventory);
+            }
+        }
     }
 }
