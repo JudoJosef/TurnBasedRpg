c0edf4517d2b7562bbfe8d1bf24226624953aae8
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 17:02:26 +0100

    Implement GiveArmor function

--- TurnBasedRPG/Dungeons/GameHandler.cs
+++ TurnBasedRPG/Dungeons/GameHandler.cs
@@ -11,6 +11,10 @@ namespace TurnBasedRPG.Dungeons
         public static void DealPhysicalDamage(ICreature user, ICreature target, int damage)
             => target.Health -= GetReducedDamage(target.Armor, damage);
 
+
+        public static void GiveArmor(List<ICreature> recievers, int amount)
+            => recievers.ForEach(reciever => ((Champion)reciever).Armor += amount);
+
         private static int GetReducedDamage(int armor, int damage)
             => damage / (1 + (armor / 100));
     }
