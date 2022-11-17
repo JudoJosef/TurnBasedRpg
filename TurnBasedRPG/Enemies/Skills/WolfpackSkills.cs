6345af39e03b7156b9a05ab1e9c35e6b9aaa854f
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 15:11:25 +0100

    Add WolfpackSkills

---
+++ TurnBasedRPG/Enemies/Skills/WolfpackSkills.cs
@@ -0,0 +1,26 @@
+﻿namespace TurnBasedRPG.Enemies.Skills
+{
+    internal class WolfpackSkills : IMonsterSkills
+    {
+        public static IEnumerable<Skill> GetSkills()
+            => new List<Skill>
+            {
+                GetFirstSkill(),
+                GetSecondSkill(),
+            };
+
+        public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        private static Skill GetFirstSkill()
+            => new Skill("On the hunt", 4, UseFirstSkill, Descriptions.Wolfpack.FirstSkill);
+
+        private static Skill GetSecondSkill()
+            => new Skill("Howl", 8, UseSecondSkill, Descriptions.Wolfpack.SecondSkill);
+    }
+}
