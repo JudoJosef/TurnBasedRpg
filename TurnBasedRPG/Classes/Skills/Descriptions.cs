f42c6f508327f7d52e6176e652029cff3dc30c1d
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 08:22:06 +0100

    Add description to assassin class

--- TurnBasedRPG/Classes/Skills/Descriptions.cs
+++ TurnBasedRPG/Classes/Skills/Descriptions.cs
@@ -8,5 +8,12 @@
             public const string SecondSkill = "Fire a splitting arrow against all target.";
             public const string ThirdSkill = "Fire multiple arrows in the air to let them rain down on all enemies.";
         }
+
+        public class Assassin
+        {
+            public const string FirstSkill = "Throw multiple knives at one enemy.";
+            public const string SecondSkill = "Random bullshit go!";
+            public const string ThirdSkill = "Instantly kill a target, except bosses.";
+        }
     }
 }
