796ffed3bcd0f7755bbff01eedbacef3037cd9dc
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 15:11:17 +0100

    Add ZombieSkills

---
+++ TurnBasedRPG/Enemies/Skills/ZombieSkills.cs
@@ -0,0 +1,26 @@
+﻿namespace TurnBasedRPG.Enemies.Skills
+{
+    internal class ZombieSkills : IMonsterSkills
+    {
+        public static IEnumerable<Skill> GetSkills()
+            => new List<Skill>
+            {
+                GetFirstSkill(),
+                GetSecondSkill(),
+            };
+
+        public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        private static Skill GetFirstSkill()
+            => new Skill("Rotten hands", 5, UseFirstSkill, Descriptions.Zombie.FirstSkill);
+
+        private static Skill GetSecondSkill()
+            => new Skill("Undead", 13, UseSecondSkill, Descriptions.Zombie.SecondSkill);
+    }
+}
