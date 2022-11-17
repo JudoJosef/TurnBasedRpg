00bed0d95f1ea7fe6a97ea69855dabcf4de0a16e
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 08:24:59 +0100

    Add ShowChampion class

--- TurnBasedRPG/Lobby/ChampionManager.cs
+++ TurnBasedRPG/Lobby/ChampionManager.cs
@@ -1,6 +1,24 @@
-﻿namespace TurnBasedRPG.Lobby
+﻿using TurnBasedRPG.Classes;
+using TurnBasedRPG.Player;
+
+namespace TurnBasedRPG.Lobby
 {
     internal class ChampionManager
     {
+        public static void ShowChampions(Summoner summoner)
+        {
+            var selected = string.Empty;
+
+            while (selected != "Back")
+            {
+                selected = Draw.SelectSingle(
+                    summoner.Champions.Select(champion => champion.Type.ToString())
+                    .Concat(new List<string> { "Back" }),
+                    "Select champion.");
+
+                if (selected != "Back")
+                    ShowChampion(GetChampion(summoner, selected));
+            }
+        }
     }
 }
