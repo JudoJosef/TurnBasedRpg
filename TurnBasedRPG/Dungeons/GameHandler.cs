aec22ac762c32076171b0382f7fc169d43417c44
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 16:53:32 +0100

    Add DealPhysicalDamage function

--- TurnBasedRPG/Dungeons/GameHandler.cs
+++ TurnBasedRPG/Dungeons/GameHandler.cs
@@ -8,5 +8,10 @@ namespace TurnBasedRPG.Dungeons
 {
     public class GameHandler
     {
+        public static void DealPhysicalDamage(ICreature user, ICreature target, int damage)
+            => target.Health -= GetReducedDamage(target.Armor, damage);
+
+        private static int GetReducedDamage(int armor, int damage)
+            => damage / (1 + (armor / 100));
     }
 }
