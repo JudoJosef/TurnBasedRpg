8b9c421bc80ede81b0e4b4ec401dec1cce190695
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 15:11:59 +0100

    Add GoblinSkills

---
+++ TurnBasedRPG/Enemies/Skills/GoblinSkills.cs
@@ -0,0 +1,28 @@
+﻿using TurnBasedRPG.Classes;
+
+namespace TurnBasedRPG.Enemies.Skills
+{
+    internal class GoblinSkills : IMonsterSkills
+    {
+        public static IEnumerable<Skill> GetSkills()
+            => new List<Skill>
+            {
+                GetFirstSkill(),
+                GetSecondSkill(),
+            };
+
+        public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        private static Skill GetFirstSkill()
+            => new Skill("Jumpattack", 3, UseFirstSkill, Descriptions.Goblin.FirstSkill);
+
+        private static Skill GetSecondSkill()
+            => new Skill("Steal", 10, UseSecondSkill, Descriptions.Goblin.SecondSkill);
+    }
+}
