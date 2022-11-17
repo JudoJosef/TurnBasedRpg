6e1672a2a87d8a5bdab501ddef0083d9616af19c
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 15:12:05 +0100

    Add GiantSkills

---
+++ TurnBasedRPG/Enemies/Skills/GiantSkills.cs
@@ -0,0 +1,26 @@
+﻿namespace TurnBasedRPG.Enemies.Skills
+{
+    internal class GiantSkills : IMonsterSkills
+    {
+        public static IEnumerable<Skill> GetSkills()
+            => new List<Skill>
+            {
+                GetFirstSkill(),
+                GetSecondSkill(),
+            };
+
+        public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
+        {
+        }
+
+        private static Skill GetFirstSkill()
+            => new Skill("Smash", 5, UseFirstSkill, Descriptions.Giant.FirstSkill);
+
+        private static Skill GetSecondSkill()
+            => new Skill("Jump", 8, UseSecondSkill, Descriptions.Giant.SecondSkill);
+    }
+}
