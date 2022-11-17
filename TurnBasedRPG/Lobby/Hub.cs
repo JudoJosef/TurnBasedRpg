1ca29dff37962daad1fc3499cead4893a2ade092
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 11:48:37 +0100

    Add switch to enter different locations

--- TurnBasedRPG/Lobby/Hub.cs
+++ TurnBasedRPG/Lobby/Hub.cs
@@ -5,5 +5,33 @@ namespace TurnBasedRPG.Lobby
 {
     public class Hub
     {
+        public void EnterLobby()
+        {
+            var selected = string.Empty;
+
+            while (true)
+            {
+                selected = Draw.SelectSingle(new List<string> { "Forge", "Shop", "Dungeon", "Exit" }, "Select option");
+                Execute(selected);
+            }
+        }
+
+        private void Execute(string selected)
+        {
+            switch (selected)
+            {
+                case "Forge":
+                    Forge.EnterForge(_summoner);
+                    break;
+                case "Shop":
+                    Shop.OpenShop(_summoner.Inventory, 1);
+                    break;
+                case "Dungeon":
+                    throw new NotImplementedException();
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+            };
+        }
     }
 }
