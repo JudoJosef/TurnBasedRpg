bd1767372609d0c20bb45986e262fc77916aa598
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 08:21:50 +0100

    Add description to archer class

---
+++ TurnBasedRPG/Classes/Skills/Descriptions.cs
@@ -0,0 +1,12 @@
+﻿namespace TurnBasedRPG.Classes.Skills
+{
+    internal class Descriptions
+    {
+        public class Archer
+        {
+            public const string FirstSkill = "Charge a powerful shot against one target.";
+            public const string SecondSkill = "Fire a splitting arrow against all target.";
+            public const string ThirdSkill = "Fire multiple arrows in the air to let them rain down on all enemies.";
+        }
+    }
+}
