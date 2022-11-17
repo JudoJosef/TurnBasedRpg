8c247890f29fb06f2d85306eb72b2710891c7333
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-17 13:54:49 +0100

    Add LobbyUtility class

---
+++ TurnBasedRPG/Lobby/LobbyUtility.cs
@@ -0,0 +1,39 @@
+﻿using TurnBasedRPG.Lobby.Items;
+
+namespace TurnBasedRPG.Lobby
+{
+    public class LobbyUtility
+    {
+        public static int GetRarityPrice(string selected)
+            => Enum.Parse<CraftingCost>(selected) switch
+            {
+                CraftingCost.Common => (int)CraftingCost.Common,
+                CraftingCost.Uncommon => (int)CraftingCost.Uncommon,
+                CraftingCost.Rare => (int)CraftingCost.Rare,
+                CraftingCost.Epic => (int)CraftingCost.Epic,
+                CraftingCost.Legendary => (int)CraftingCost.Legendary,
+                CraftingCost.Mythic => (int)CraftingCost.Mythic,
+                _ => throw new Exception(),
+            };
+
+        public static int GetId(List<int> usedIds)
+        {
+            var availableId = -1;
+            usedIds.ForEach(id =>
+            {
+                if (id >= availableId)
+                    availableId = id + 1;
+            });
+
+            return availableId == -1
+                ? 0
+                : availableId;
+        }
+
+        public static Item GetItem(string selected, Dictionary<int, Item> items)
+            => items[int.Parse(
+                selected.Split(" ")
+                .First()
+                .Replace("#", string.Empty))];
+    }
+}
