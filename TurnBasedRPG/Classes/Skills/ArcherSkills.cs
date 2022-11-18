0a6a4c274d22ff85fc298139bc39cb0df25b8f8b
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-18 08:15:09 +0100

    Implement archer skills

--- TurnBasedRPG/Classes/Skills/ArcherSkills.cs
+++ TurnBasedRPG/Classes/Skills/ArcherSkills.cs
@@ -1,4 +1,5 @@
-﻿using TurnBasedRPG.Enemies;
+﻿using TurnBasedRPG.Dungeons;
+using TurnBasedRPG.Enemies;
 
 namespace TurnBasedRPG.Classes.Skills
 {
@@ -12,16 +13,23 @@ namespace TurnBasedRPG.Classes.Skills
                 GetThirdSkill(),
             };
 
-        public static void UseFirstSkill(Champion champion, List<ICreature> creatures)
+        public static void UseFirstSkill(ICreature champion, List<ICreature> creatures)
         {
+            var target = GameHandler.GetTarget(creatures);
+            var damage = ((Champion)champion).Strength * 3;
+            GameHandler.DealPhysicalDamage(target, damage);
         }
 
-        public static void UseSecondSkill(Champion champion, List<ICreature> creatures)
+        public static void UseSecondSkill(ICreature champion, List<ICreature> creatures)
         {
+            var damage = (int)(((Champion)champion).Strength * 1.5);
+            creatures.ForEach(target => GameHandler.DealPhysicalDamage(target, damage));
         }
 
-        public static void UseThirdSkill(Champion champion, List<ICreature> creatures)
+        public static void UseThirdSkill(ICreature champion, List<ICreature> creatures)
         {
+            var damage = ((Champion)champion).Strength * 4;
+            creatures.ForEach(target => GameHandler.DealPhysicalDamage(target, damage));
         }
 
         private static Skill GetFirstSkill()
--- TurnBasedRPG/Classes/Skills/Descriptions.cs
+++ TurnBasedRPG/Classes/Skills/Descriptions.cs
@@ -5,7 +5,7 @@
         public class Archer
         {
             public const string FirstSkill = "Charge a powerful shot against one target.";
-            public const string SecondSkill = "Fire a splitting arrow against all target.";
+            public const string SecondSkill = "Fire a splitting arrow against all targets.";
             public const string ThirdSkill = "Fire multiple arrows in the air to let them rain down on all enemies.";
         }
 
