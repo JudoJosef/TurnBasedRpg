3cd1823f7d9b15de5fcf12917bad21bf71a2a024
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-24 15:56:06 +0100

    Fix messages in AssassinSkills class

--- TurnBasedRPG/Classes/Skills/AssassinSkills.cs
+++ TurnBasedRPG/Classes/Skills/AssassinSkills.cs
@@ -19,7 +19,7 @@ namespace TurnBasedRPG.Classes.Skills
             var damage = champion.Strength * 3;
             GameHandler.DealPhysicalDamage(target, damage);
             GameHandler.SetCooldown(champion, 0);
-            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(champion, target, champion.Skills.First().Name));
+            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(((IAlly)champion).Type, ((IMonster)target).Type, champion.Skills.First().Name));
         }
 
         public static void UseSecondSkill(ICreature champion, List<ICreature> creatures)
@@ -27,7 +27,7 @@ namespace TurnBasedRPG.Classes.Skills
             var damage = (int)(champion.Strength * 1.8);
             creatures.ForEach(target => GameHandler.DealPhysicalDamage(target, damage));
             GameHandler.SetCooldown(champion, 1);
-            Draw.WriteLineAndWait(Messages.UseAOESkill(champion, champion.Skills.ElementAt(1).Name));
+            Draw.WriteLineAndWait(Messages.UseAOESkill(((IAlly)champion).Type, champion.Skills.ElementAt(1).Name));
         }
 
         public static void UseThirdSkill(ICreature champion, List<ICreature> creatures)
@@ -40,7 +40,7 @@ namespace TurnBasedRPG.Classes.Skills
                 GameHandler.DealPhysicalDamage(target, damage);
 
             GameHandler.SetCooldown(champion, 2);
-            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(champion, target, champion.Skills.Last().Name));
+            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(((IAlly)champion).Type, ((IMonster)target).Type, champion.Skills.Last().Name));
         }
 
         private static Skill GetFirstSkill()
