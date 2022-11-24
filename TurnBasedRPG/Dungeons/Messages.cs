8947e56e611e8a21ee26bfe1578b06932dbf0e2b
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-24 15:45:08 +0100

    Change type in Messages functions

--- TurnBasedRPG/Dungeons/Messages.cs
+++ TurnBasedRPG/Dungeons/Messages.cs
@@ -2,34 +2,34 @@
 {
     public class Messages
     {
-        public static Func<ICreature, ICreature, string> DamageTarget =
+        public static Func<Enum, Enum, string> DamageTarget =
             (creature, target) => $"{creature} has attacked {target}";
 
-        public static Func<ICreature, ICreature, string> HealTarget =
+        public static Func<Enum, Enum, string> HealTarget =
             (creature, target) => $"{creature} has healed {target}";
 
-        public static Func<ICreature, string> ReviveTarget =
+        public static Func<Enum, string> ReviveTarget =
             (target) => $"{target} has been revived";
 
-        public static Func<ICreature, ICreature, string, string> UseSingleTargetSkill =
+        public static Func<Enum, Enum, string, string> UseSingleTargetSkill =
             (creature, target, skillname) => $"{creature} has used {skillname} against {target}";
 
-        public static Func<ICreature, string, string> UseAOESkill =
+        public static Func<Enum, string, string> UseAOESkill =
             (creature, skillname) => $"{creature} has used {skillname}";
 
-        public static Func<ICreature, string> Defeated =
+        public static Func<Enum, string> Defeated =
             (creature) => $"{creature} has been defeated";
 
-        public static Func<ICreature, string> IncreaseStats =
+        public static Func<Enum, string> IncreaseStats =
             (creature) => $"The stats of {creature} have been increased";
 
-        public static Func<ICreature, ICreature, int, string> DebuffTarget =
+        public static Func<Enum, Enum, int, string> DebuffTarget =
             (creature, target, rounds) => $"{creature} has debuffed {target} for {rounds} rounds";
 
-        public static Func<ICreature, ICreature, string> StealItem =
+        public static Func<Enum, Enum, string> StealItem =
             (creature, target) => $"{creature} has an item from {target}";
 
-        public static Func<ICreature, string> GetShield =
+        public static Func<Enum, string> GetShield =
             (creature) => $"{creature} has recieved a shield";
     }
 }
