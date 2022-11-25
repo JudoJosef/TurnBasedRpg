f93028d4b94ad6c4f6f21631c9295449a5105531
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-25 13:29:00 +0100

    Nerf DragonSkills

--- TurnBasedRPG/Dungeons/Enemies/Skills/DragonSkills.cs
+++ TurnBasedRPG/Dungeons/Enemies/Skills/DragonSkills.cs
@@ -11,7 +11,7 @@
 
         public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
         {
-            var damage = monster.Strength * 4;
+            var damage = monster.Strength * 2;
             targets.ForEach(target => GameHandler.DealPhysicalDamage(target, damage));
             GameHandler.SetCooldown(monster, 0);
             Draw.WriteLineAndWait(Messages.UseAOESkill(((IMonster)monster).Type, monster.Skills.First().Name));
@@ -19,7 +19,7 @@
 
         public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
         {
-            var damage = monster.Strength * 8;
+            var damage = monster.Strength * 5;
             targets.ForEach(target => GameHandler.DealPhysicalDamage(target, damage));
             GameHandler.SetCooldown(monster, 1);
             Draw.WriteLineAndWait(Messages.UseAOESkill(((IMonster)monster).Type, monster.Skills.Last().Name));
