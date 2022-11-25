4e6025fd6ea7b43d1485eaf9cb51735ff2731f04
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-11-25 11:39:19 +0100

    Increase base resistance and health for champions

--- TurnBasedRPG/Classes/Portal.cs
+++ TurnBasedRPG/Classes/Portal.cs
@@ -6,72 +6,72 @@ namespace TurnBasedRPG.Classes
     {
         public static Champion SummonArcher()
             => new(
-                70,
-                20,
-                10,
+                400,
+                30,
+                30,
                 30,
                 ArcherSkills.GetSkills(),
                 ClassTypes.Archer);
 
         public static Champion SummonAssassin()
             => new(
-                80,
-                20,
-                10,
+                420,
+                35,
+                30,
                 70,
                 AssassinSkills.GetSkills(),
                 ClassTypes.Assassin);
 
         public static Champion SummonDryad()
             => new(
-                100,
-                25,
-                20,
+                500,
+                30,
+                40,
                 25,
                 DryadSkills.GetSkills(),
                 ClassTypes.Dryad);
 
         public static Champion SummonFighter()
             => new(
-                140,
-                40,
-                30,
+                290,
+                70,
+                60,
                 60,
                 FighterSkills.GetSkills(),
                 ClassTypes.Fighter);
 
         public static Champion CallJojo()
             => new(
-                200,
-                50,
-                50,
+                1100,
+                110,
+                110,
                 90,
                 JojoSkills.GetSkills(),
                 ClassTypes.Jojo);
 
         public static Champion SummonMage()
             => new(
-                80,
-                25,
-                40,
+                430,
+                35,
+                65,
                 30,
                 MageSkills.GetSkills(),
                 ClassTypes.Mage);
 
         public static Champion SummonPaladin()
             => new(
-                400,
-                65,
-                65,
+                1500,
+                100,
+                100,
                 35,
                 PaladinSkills.GetSkills(),
                 ClassTypes.Paladin);
 
         public static Champion SummonSwordsman()
             => new(
-                210,
-                45,
-                20,
+                760,
+                75,
+                40,
                 75,
                 SwordsmanSkills.GetSkills(),
                 ClassTypes.Swordsman);
