4fdba825b9fad418690b9ee52d9ab9cbe8b071e9
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-01 08:50:55 +0100

    Implement SoulEaterSkills

---
+++ TurnBasedRPG/Dungeons/Enemies/Skills/SoulEaterSkills.cs
@@ -0,0 +1,55 @@
+﻿using TurnBasedRPG.Classes;
+
+namespace TurnBasedRPG.Dungeons.Enemies.Skills
+{
+    internal class SoulEaterSkills : IMonsterSkills
+    {
+        public static List<Skill> GetSkills()
+            => new List<Skill>
+            {
+                GetFirstSkill(),
+                GetSecondSkill(),
+                GetThirdSkill(),
+            };
+
+        public static void UseFirstSkill(ICreature monster, List<ICreature> targets)
+        {
+            var target = GameHandler.GetRandomTarget(targets);
+            var damage = monster.Strength * 6;
+            GameHandler.DealMagicDamage(target, damage);
+            GameHandler.SetCooldown(monster, 0);
+            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(((IMonster)monster).Type, ((IAlly)target).Type, monster.Skills.First().Name));
+        }
+
+        public static void UseSecondSkill(ICreature monster, List<ICreature> targets)
+        {
+            monster.Health = (int)(monster.Health * 1.5);
+            monster.MaxHealth = (int)(monster.MaxHealth * 1.5);
+            monster.Strength = (int)(monster.Strength * 1.5);
+            monster.Armor = (int)(monster.Armor * 1.5);
+            monster.MagicDefense = (int)(monster.MagicDefense * 1.5);
+            GameHandler.SetCooldown(monster, 1);
+            Draw.WriteLineAndWait(Messages.IncreaseStats(((IMonster)monster).Type));
+        }
+
+        public static void UseThirdSkill(ICreature monster, List<ICreature> targets)
+        {
+            var target = GameHandler.GetRandomTarget(targets);
+            var damage = target.Health;
+            GameHandler.DealPhysicalDamage(target, damage);
+            var actualDamage = damage - target.Health;
+            GameHandler.HealCreature(monster, actualDamage);
+            GameHandler.SetCooldown(monster, 2);
+            Draw.WriteLineAndWait(Messages.UseSingleTargetSkill(((IMonster)monster).Type, ((IAlly)target).Type, monster.Skills.Last().Name));
+        }
+
+        private static Skill GetFirstSkill()
+            => new Skill("Soul collector", 5, UseFirstSkill, Descriptions.SoulEater.FirstSkill);
+
+        private static Skill GetSecondSkill()
+            => new Skill("Warden of the dead", 13, UseSecondSkill, Descriptions.SoulEater.SecondSkill);
+
+        private static Skill GetThirdSkill()
+            => new Skill("Devour", 17, UseSecondSkill, Descriptions.SoulEater.ThirdSkill);
+    }
+}
