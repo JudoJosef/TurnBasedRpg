de2fd45e01e96992d8be7d1f22e6c4966e16a6bc
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-02 14:05:49 +0100

    Add cost for item upgrades

--- TurnBasedRPG/Lobby/Items/CraftingCost.cs
+++ TurnBasedRPG/Lobby/Items/CraftingCost.cs
@@ -9,4 +9,14 @@
         Legendary = 500,
         Mythic = 1000,
     }
+
+    public enum ItemPrice
+    {
+        Common = 100,
+        Uncommon = 175,
+        Rare = 300,
+        Epic = 500,
+        Legendary = 800,
+        Mythic = 1500,
+    }
 }
