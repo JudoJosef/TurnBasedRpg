b957317b37bf141415381e9ca9f2de250f3b34a6
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-09 13:18:39 +0100

    Refactor BookOfMonsters class

--- TurnBasedRPG/Lobby/BookOfMonsters.cs
+++ TurnBasedRPG/Lobby/BookOfMonsters.cs
@@ -1,90 +1,99 @@
 ﻿using TurnBasedRPG.Player;
 using static TurnBasedRPG.Constants;
 
-namespace TurnBasedRPG.Lobby
+namespace TurnBasedRPG.Lobby;
+
+public class BookOfMonsters
 {
-    public class BookOfMonsters
+    private Summoner _summoner;
+
+    public BookOfMonsters(Summoner summoner)
     {
-        private Summoner _summoner;
+        _summoner = summoner;
+    }
 
-        public BookOfMonsters(Summoner summoner)
+    public void Open()
+    {
+        var selected = string.Empty;
+
+        while (selected != BackOption)
         {
-            _summoner = summoner;
-        }
+            selected = UiReferencer.SelectSingle(
+                new List<string> { MonstersOption, BossesOption, BackOption },
+                SelectCategoryCaption);
 
-        public void Open()
-        {
-            var selected = string.Empty;
-
-            while (selected != BackOption)
+            if (selected == MonstersOption)
             {
-                selected = UiReferencer.SelectSingle(
-                    new List<string> { MonstersOption, BossesOption, BackOption },
-                    SelectCategoryCaption);
-
-                if (selected == MonstersOption)
-                    ShowMonsters();
-                else if (selected == BossesOption)
-                    ShowBosses();
+                ShowMonsters();
+            }
+            else if (selected == BossesOption)
+            {
+                ShowBosses();
             }
         }
+    }
 
-        private void ShowMonsters()
+    private void ShowMonsters()
+    {
+        var selected = string.Empty;
+
+        while (selected != BackOption)
         {
-            var selected = string.Empty;
+            selected = UiReferencer.SelectSingle(
+                _summoner.DefeatedCreatures.Where(creature =>
+                    (int)creature.Type < 30)
+                .Select(monster =>
+                    monster.Type.ToString())
+                .Concat(new List<string> { BackOption }),
+                SelectMonsterCaption);
 
-            while (selected != BackOption)
+            if (selected != BackOption)
             {
-                selected = UiReferencer.SelectSingle(
-                    _summoner.DefeatedCreatures.Where(creature =>
-                        (int)creature.Type < 30)
-                    .Select(monster =>
-                        monster.Type.ToString())
-                    .Concat(new List<string> { BackOption }),
-                    SelectMonsterCaption);
-
-                if (selected != BackOption)
-                    ShowCreature(selected);
+                ShowCreature(selected);
             }
         }
+    }
 
-        private void ShowBosses()
+    private void ShowBosses()
+    {
+        var selected = string.Empty;
+
+        while (selected != BackOption)
         {
-            var selected = string.Empty;
+            selected = UiReferencer.SelectSingle(
+                _summoner.DefeatedCreatures.Where(creature =>
+                    (int)creature.Type > 29)
+                .Select(monster =>
+                    monster.Type.ToString())
+                .Concat(new List<string> { BackOption }),
+                SelectBossCaption);
 
-            while (selected != BackOption)
+            if (selected != BackOption)
             {
-                selected = UiReferencer.SelectSingle(
-                    _summoner.DefeatedCreatures.Where(creature =>
-                        (int)creature.Type > 29)
-                    .Select(monster =>
-                        monster.Type.ToString())
-                    .Concat(new List<string> { BackOption }),
-                    SelectBossCaption);
-
-                if (selected != BackOption)
-                    ShowCreature(selected);
+                ShowCreature(selected);
             }
         }
+    }
 
-        private void ShowCreature(string type)
+    private void ShowCreature(string type)
+    {
+        var creature = _summoner.DefeatedCreatures.Where(creature => creature.Type.ToString() == type).Single();
+
+        var selected = string.Empty;
+
+        while (selected != BackOption)
         {
-            var creature = _summoner.DefeatedCreatures.Where(creature => creature.Type.ToString() == type).Single();
+            UiReferencer.Clear();
+            selected = UiReferencer.SelectSingle(
+                creature.Skills.Select(skill => skill.Name)
+                .Concat(new List<string> { BackOption }),
+                $"{type}\n{SelectSkillCaption}");
 
-            var selected = string.Empty;
-
-            while (selected != BackOption)
+            if (selected != BackOption)
             {
-                UiReferencer.Clear();
-                selected = UiReferencer.SelectSingle(
-                    creature.Skills.Select(skill => skill.Name)
-                    .Concat(new List<string> { BackOption }),
-                    $"{type}\n{SelectSkillCaption}");
-
-                if (selected != BackOption)
-                    UiReferencer.WriteLineAndWait(
-                        creature.Skills.Where(skill => skill.Name == selected)
-                        .Single().Description);
+                UiReferencer.WriteLineAndWait(
+                    creature.Skills.Where(skill => skill.Name == selected)
+                    .Single().Description);
             }
         }
     }
