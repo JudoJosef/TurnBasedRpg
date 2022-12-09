b3d7f9abb8ee7b8da40b8fddb2a5361ff338aec4
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-09 13:15:59 +0100

    Refactor UiReferencer

--- TurnBasedRPG/UiReferencer.cs
+++ TurnBasedRPG/UiReferencer.cs
@@ -1,41 +1,38 @@
-﻿using Spectre.Console;
-using TurnBasedRPG.Classes;
-using TurnBasedRPG.Dungeons.Enemies;
+﻿using TurnBasedRPG.Champions;
 using TurnBasedRPG.Lobby.Items;
 using TurnBasedRPG.Player;
 
-namespace TurnBasedRPG
+namespace TurnBasedRPG;
+
+public static class UiReferencer
 {
-    public static class UiReferencer
-    {
-        public static string SelectSingle(IEnumerable<string> options, string title)
-            => Draw.SelectSingle(options, title);
+    public static string SelectSingle(IEnumerable<string> options, string title)
+        => Draw.SelectSingle(options, title);
 
-        public static string GetLine(string line)
-            => Draw.GetLine(line);
+    public static string GetLine(string line)
+        => Draw.GetLine(line);
 
-        public static void Clear()
-            => Draw.Clear();
+    public static void Clear()
+        => Draw.Clear();
 
-        public static void WriteLine(string line)
-            => Draw.WriteLine(line);
+    public static void WriteLine(string line)
+        => Draw.WriteLine(line);
 
-        public static void WriteLineAndWait(string line)
-            => Draw.WriteLineAndWait(line);
+    public static void WriteLineAndWait(string line)
+        => Draw.WriteLineAndWait(line);
 
-        public static void WriteLootTable(ItemRarity rarity, SummonerInventory inventory, int craftingCost)
-            => Draw.WriteLootTable(rarity, inventory, craftingCost);
+    public static void WriteLootTable(ItemRarity rarity, SummonerInventory inventory, int craftingCost)
+        => Draw.WriteLootTable(rarity, inventory, craftingCost);
 
-        public static void WriteItemTable(List<Item> items)
-            => Draw.WriteItemTable(items);
+    public static void WriteItemTable(List<Item> items)
+        => Draw.WriteItemTable(items);
 
-        public static void WriteChampionStatTable(List<Champion> champions)
-            => Draw.WriteChampionStatTable(champions);
+    public static void WriteChampionStatTable(List<Champion> champions)
+        => Draw.WriteChampionStatTable(champions);
 
-        public static void WriteChampionFightStatTable(List<Champion> champions)
-            => Draw.WriteChampionFightStatTable(champions);
+    public static void WriteChampionFightStatTable(List<Champion> champions)
+        => Draw.WriteChampionFightStatTable(champions);
 
-        public static void WriteMonsterStatTable(List<ICreature> creatures)
-            => Draw.WriteMonsterStatTable(creatures);
-    }
+    public static void WriteMonsterStatTable(List<ICreature> creatures)
+        => Draw.WriteMonsterStatTable(creatures);
 }
