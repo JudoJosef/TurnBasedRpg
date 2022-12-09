cfce12022a614be3209fa1bfcd1798e9ef231a5c
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-09 13:18:56 +0100

    Refactor ICreature class

--- TurnBasedRPG/ICreature.cs
+++ TurnBasedRPG/ICreature.cs
@@ -1,21 +1,28 @@
-﻿using TurnBasedRPG.Classes;
+﻿using TurnBasedRPG.Champions;
 
-namespace TurnBasedRPG
+namespace TurnBasedRPG;
+
+public interface ICreature
 {
-    public interface ICreature
-    {
-        int Health { get; set; }
-        int MaxHealth { get; set; }
-        int Armor { get; set; }
-        int MagicDefense { get; set; }
-        int Strength { get; set; }
+    int Health { get; set; }
 
-        List<Skill> Skills { get; }
-        List<Debuff> Debuffs { get; set; }
+    int MaxHealth { get; set; }
 
-        void TurnAction(List<ICreature> creatures);
-        void Attack(ICreature creature);
-        void UseSkill(List<ICreature> creatures);
-        void Die();
-    }
+    int Armor { get; set; }
+
+    int MagicDefense { get; set; }
+
+    int Strength { get; set; }
+
+    List<Skill> Skills { get; }
+
+    List<Debuff> Debuffs { get; set; }
+
+    void TurnAction(List<ICreature> creatures);
+
+    void Attack(ICreature creature);
+
+    void UseSkill(List<ICreature> creatures);
+
+    void Die();
 }
