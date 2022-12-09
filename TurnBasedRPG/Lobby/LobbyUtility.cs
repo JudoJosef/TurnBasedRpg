9373965db45bdc5a6508ef543e7469c802e3a637
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-09 13:17:29 +0100

    Refactor LobbyUtility class

--- TurnBasedRPG/Lobby/LobbyUtility.cs
+++ TurnBasedRPG/Lobby/LobbyUtility.cs
@@ -1,51 +1,52 @@
 ﻿using TurnBasedRPG.Lobby.Items;
 
-namespace TurnBasedRPG.Lobby
+namespace TurnBasedRPG.Lobby;
+
+public class LobbyUtility
 {
-    public class LobbyUtility
-    {
-        public static int GetRarityCraftingPrice(string selected)
-            => Enum.Parse<CraftingCost>(selected) switch
-            {
-                CraftingCost.Common => (int)CraftingCost.Common,
-                CraftingCost.Uncommon => (int)CraftingCost.Uncommon,
-                CraftingCost.Rare => (int)CraftingCost.Rare,
-                CraftingCost.Epic => (int)CraftingCost.Epic,
-                CraftingCost.Legendary => (int)CraftingCost.Legendary,
-                CraftingCost.Mythic => (int)CraftingCost.Mythic,
-                _ => throw new Exception(),
-            };
-
-        public static int GetRarityPrice(string selected)
-            => Enum.Parse<CraftingCost>(selected) switch
-            {
-                CraftingCost.Common => (int)ItemPrice.Common,
-                CraftingCost.Uncommon => (int)ItemPrice.Uncommon,
-                CraftingCost.Rare => (int)ItemPrice.Rare,
-                CraftingCost.Epic => (int)ItemPrice.Epic,
-                CraftingCost.Legendary => (int)ItemPrice.Legendary,
-                CraftingCost.Mythic => (int)ItemPrice.Mythic,
-                _ => throw new Exception(),
-            };
-
-        public static int GetId(List<int> usedIds)
+    public static int GetRarityCraftingPrice(string selected)
+        => Enum.Parse<CraftingCost>(selected) switch
         {
-            var availableId = -1;
-            usedIds.ForEach(id =>
+            CraftingCost.Common => (int)CraftingCost.Common,
+            CraftingCost.Uncommon => (int)CraftingCost.Uncommon,
+            CraftingCost.Rare => (int)CraftingCost.Rare,
+            CraftingCost.Epic => (int)CraftingCost.Epic,
+            CraftingCost.Legendary => (int)CraftingCost.Legendary,
+            CraftingCost.Mythic => (int)CraftingCost.Mythic,
+            _ => throw new Exception(),
+        };
+
+    public static int GetRarityPrice(string selected)
+        => Enum.Parse<CraftingCost>(selected) switch
+        {
+            CraftingCost.Common => (int)ItemPrice.Common,
+            CraftingCost.Uncommon => (int)ItemPrice.Uncommon,
+            CraftingCost.Rare => (int)ItemPrice.Rare,
+            CraftingCost.Epic => (int)ItemPrice.Epic,
+            CraftingCost.Legendary => (int)ItemPrice.Legendary,
+            CraftingCost.Mythic => (int)ItemPrice.Mythic,
+            _ => throw new Exception(),
+        };
+
+    public static int GetId(List<int> usedIds)
+    {
+        var availableId = -1;
+        usedIds.ForEach(id =>
+        {
+            if (id >= availableId)
             {
-                if (id >= availableId)
-                    availableId = id + 1;
-            });
+                availableId = id + 1;
+            }
+        });
 
-            return availableId == -1
-                ? 0
-                : availableId;
-        }
-
-        public static Item GetItem(string selected, Dictionary<int, Item> items)
-            => items[int.Parse(
-                selected.Split(" ")
-                .First()
-                .Replace("#", string.Empty))];
+        return availableId == -1
+            ? 0
+            : availableId;
     }
+
+    public static Item GetItem(string selected, Dictionary<int, Item> items)
+        => items[int.Parse(
+            selected.Split(" ")
+            .First()
+            .Replace("#", string.Empty))];
 }
