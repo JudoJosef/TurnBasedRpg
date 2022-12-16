badcb17410655daae0f023ceaff32e19f280481c
Patrick Perathoner <patrick.perathoner@messerli.ch>
2022-12-16 13:29:05 +0100

    Add and implement Miner class

---
+++ TurnBasedRPG/Lobby/Miner.cs
@@ -0,0 +1,35 @@
+﻿namespace TurnBasedRPG.Lobby;
+public class Miner
+{
+    public int MiningForce { get; set; } = 1;
+
+    public int MinedGold { get; set; }
+
+    public int UpgradeCost { get; set; } = 100;
+
+    public int Level { get; set; } = 1;
+
+    public void Upgrade()
+    {
+        MiningForce++;
+        Level++;
+        UpgradeCost += 10;
+    }
+
+    public async void Work()
+    {
+        while (true)
+        {
+            MinedGold += MiningForce;
+            await Task.Delay(1000);
+        }
+    }
+
+    public int DeliverGold()
+    {
+        var gold = MinedGold;
+        MinedGold = 0;
+
+        return gold;
+    }
+}
